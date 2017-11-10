7472985583a9a511cecd65222218f48b49163d6d
Michael L Janner <janner@openmind7.mit.edu>
2017-11-10 16:55:47 -0500

    composer

--- dataset/divide.py
+++ dataset/divide.py
@@ -2,15 +2,15 @@ import os, argparse, subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--low',        default=0,                      type=int,   help='min image index')
-parser.add_argument('--high',       default=20000,                  type=int,   help='max image index')
+parser.add_argument('--high',       default=200,                  type=int,   help='max image index')
 parser.add_argument('--repeat',     default=10,                      type=int,   help='number of renderings per object')
-parser.add_argument('--division',   default=50,                     type=int,   help='number of images per SLURM job')
+parser.add_argument('--division',   default=10,                     type=int,   help='number of images per SLURM job')
 parser.add_argument('--qos',        default=False,                  type=bool,  help='whether to use quality-of-service queue')
 parser.add_argument('--jobs',       default=None,                               help='list of (low, high) indices to render, overrides {low,high,repeat,division}')
 parser.add_argument('--script',     default='run.py',               type=str,   help='script to launch blender job')
 parser.add_argument('--mode',       default='render.py',            type=str,   help='script run within blender')
 parser.add_argument('--category',   default='car',            type=str,   help='object category (from ShapeNet or primitive, see options in config.py)')
-parser.add_argument('--output',     default='output/car_train/',    type=str,   help='save folder')
+parser.add_argument('--output',     default='output/car_val/',    type=str,   help='save folder')
 args = parser.parse_args()
 
 
